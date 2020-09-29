f7d49422bd0c834bddf3cc3b73f28036ef3d1fb0
Atsushi-Eda <aeda0922@gmail.com>
2020-09-29 11:32:48 +0900

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist')
